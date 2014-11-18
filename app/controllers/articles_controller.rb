9023cf0ee71e3cb79a2cd7ac2a1c5cd65178ed81
iamcooc <yangyunchang@Yangyuns-iMac.local>
2014-11-18 04:27:41 -0500

    error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     
-    @article = current_user.articles.build(micropost_params)
+    @article = current_user.articles.build(article_params)
 
     respond_to do |format|
       if @article.save
