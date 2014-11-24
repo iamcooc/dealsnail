0450e065bac309407a81d2bcc4a7fe30f1d410ee
iamcooc <yangyunchang@Yangyuns-iMac.local>
2014-11-24 02:37:28 -0500

    some changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,12 @@ class StaticPagesController < ApplicationController
 
 
 	def home
-		@articles = Article.all.paginate(:page => params[:page], :per_page => 3)
+		@articles = Article.all.paginate(:page => params[:page], :per_page => 20)
 	end	
 
 	def search
 		if params[:query].present?
-			@articles = Article.search(params[:query], page: params[:page], :per_page => 3)
+			@articles = Article.search(params[:query], page: params[:page], :per_page => 30)
 			@query = params[:query]
 		else
 			@result = "no"
